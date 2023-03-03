9622834399905ee50890a1ddb8ce27b84ea80614
fadlifrl <fadlifirdass@gmail.com>
2023-03-03 10:42:35 +0700

    login, navbar, dashboard, routes with dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Login from "./components/login/Login";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Navbar from "./containers/Navbar";
 
 function App() {
   return (
-    <div>
-        <Login/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>}/> 
+      <Route path="/navbar" element={<Navbar/>}/> 
+   </Routes>
+   </BrowserRouter>
   );
 }
 
